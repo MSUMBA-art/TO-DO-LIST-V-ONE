41ef160cb10eb9aabfe96faeaaadb003590cba5c
ALEXANDER <msumba83@gmail.com>
2022-03-13 22:08:14 +0200

    Passing data from Server to a  Webpage

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+
 const app = express();
+
+var items = ["Buy Food", "Eat", "Sleep" ];
 
 app.set("view engine", "ejs");
 
@@ -10,42 +13,7 @@ const port = 3000;
 
 app.get("/", function(req, res)  {
   var today = new Date();
-  // var currentDay = today.getDay();
-  // var day = "";
-
-  // switch (currentDay) {
-  //   case 0:
-  //     day = "Sunday";
-  //     break;
-
-  //   case 1:
-  //     day = "Monday";
-  //     break;
-
-  //   case 2:
-  //     day = "Tuesday";
-  //     break;
-
-  //   case 3:
-  //     day = "Wednesday";
-  //     break;
-
-  //   case 4:
-  //     day = "Thursday";
-  //     break;
-
-  //   case 5:
-  //     day = "Friday";
-  //     break;
-
-  //   case 6:
-  //     day = "Saturday";
-  //     break;
-
-  //   default:
-
-  //   console.log("Error: current day is" + currentDay);
-  
+ 
   var options = {
     weekday: "long",
     day: "numeric",
@@ -54,13 +22,16 @@ app.get("/", function(req, res)  {
 
   var day = today.toLocaleDateString("en-US", options);
 
-  res.render("list", { kindOfDay: day });
+  res.render("list", { kindOfDay: day, newListItems: items });
 });
 
-app.post("/", function(req, send) {
-  
-  console.log(req.body.newItem)
-} )
+app.post("/", function(req, res) {
+  var item = req.body.newItem;
+
+  items.push(item);
+
+  res.redirect("/");
+} );
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
