4e110cb306c22bbc034ada74aeecf6eb87dd6a22
ALEXANDER <msumba83@gmail.com>
2022-03-14 06:30:06 +0200

    scope

--- app.js
+++ app.js
@@ -3,35 +3,35 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-var items = ["Buy Food", "Eat", "Sleep" ];
+let items = ["Buy Food", "Eat", "Sleep"];
 
 app.set("view engine", "ejs");
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const port = 3000;
 
-app.get("/", function(req, res)  {
-  var today = new Date();
- 
-  var options = {
+app.get("/", function (req, res) {
+  let today = new Date();
+
+  let options = {
     weekday: "long",
     day: "numeric",
-    month: "long"
+    month: "long",
   };
 
-  var day = today.toLocaleDateString("en-US", options);
+  let day = today.toLocaleDateString("en-US", options);
 
   res.render("list", { kindOfDay: day, newListItems: items });
 });
 
-app.post("/", function(req, res) {
-  var item = req.body.newItem;
+app.post("/", function (req, res) {
+  let item = req.body.newItem;
 
   items.push(item);
 
   res.redirect("/");
-} );
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
