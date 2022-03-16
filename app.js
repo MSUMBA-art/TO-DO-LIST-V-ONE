f85433a39fcdd6719f2eccbc997a267774d47ce7
ALEXANDER <msumba83@gmail.com>
2022-03-16 16:11:57 +0200

    templating and layout

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser");
 const app = express();
 
 let items = ["Buy Food", "Eats", "Sleep"];
+let workItems = [];
 
 app.set("view engine", "ejs");
 
@@ -26,15 +27,22 @@ app.get("/", function (req, res) {
 
   let day = today.toLocaleDateString("en-US", options);
 
-  res.render("list", { kindOfDay: day, newListItems: items });
+  res.render("list", { listTitle: day, newListItems: items });
 });
 
 app.post("/", function (req, res) {
   let item = req.body.newItem;
 
-  items.push(item);
-
-  res.redirect("/");
+  if (req.body.list === "work") {
+    workItems.push(item);
+    res.redirect("work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
+});
+app.get("/work", function (req, res) {
+  res.render("list", { listTitle: "work List", newListItems: workItems });
 });
 
 app.listen(port, () => {
